d431f27b348ff794ae63967dde74188114fcb0cc
BergTim <timon@bergmann.ch>
2019-11-25 14:54:18 +0100

    added init function

---
+++ HWO/max7219.c
@@ -0,0 +1,19 @@
+/*
+ * max7219.c
+ *
+ *  Created on: 25.11.2019
+ *      Author: timon
+ */
+
+static f_ptr_u8 spi_tx = 0;
+static f_ptr_b chip_sel = 0;
+
+void max7219_init(f_ptr_u8 spi_transmit, f_ptr_b chipselect)
+{
+    spi_tx = spi_transmit;
+    chip_sel = chipselect;
+}
+
+
+
+
---
+++ HWO/max7219.h
@@ -0,0 +1,13 @@
+/*
+ * max7219.h
+ *
+ *  Created on: 25.11.2019
+ *      Author: timon
+ */
+
+#ifndef HWO_MAX7219_H_
+#define HWO_MAX7219_H_
+
+
+
+#endif /* HWO_MAX7219_H_ */
